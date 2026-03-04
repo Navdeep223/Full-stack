410f1a5ad4d4904db5c43e3bfa43486a1b2ff7ec
Navdeep223 <ns3321960@gmail.com>
2026-03-04 19:26:40 +0530

    Fix controller path

--- reddis_locking_system/src/modules/booking/booking.route.js
+++ reddis_locking_system/src/modules/booking/booking.route.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { bookSeatController } from "../controllers/booking.controller.js";
+import { bookSeatController } from "./booking.controller.js";
 
 const router = express.Router();
 
-router.get("/book/:seatId", (req,res)=>{
-    const seatId = req.params.seatId;
-    res.json({message:`Seat ${seatId} booked successfully.`});
+router.get("/book/:seatId", (req, res) => {
+  const seatId = req.params.seatId;
+  res.json({ message: `Seat ${seatId} booked successfully.` });
 });
 
 router.post("/book/:seatId", bookSeatController);
